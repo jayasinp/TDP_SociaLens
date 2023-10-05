9958bd670cd1a7169f8fc0d3461e31c2f25b21f3
jayasinp <pravinjayasinghe@gmail.com>
2023-10-05 18:45:12 +1100

    updated with external component switching

--- pages/demodashboard.js
+++ pages/demodashboard.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Navbar from "../components/Navbar";
 import Home from "../dashboardComponents/Home";
 import Reports from "../dashboardComponents/Reports";
 
@@ -7,22 +8,7 @@ function Dashboard() {
 
   return (
     <div className="container">
-      <div className="btn-group" role="group">
-        <button
-          type="button"
-          className={`btn btn-secondary ${view === "home" ? "active" : ""}`}
-          onClick={() => setView("home")}
-        >
-          Home
-        </button>
-        <button
-          type="button"
-          className={`btn btn-secondary ${view === "reports" ? "active" : ""}`}
-          onClick={() => setView("reports")}
-        >
-          Reports
-        </button>
-      </div>
+      <Navbar view={view} setView={setView} />
       <div className="mt-4">
         {view === "home" && <Home />}
         {view === "reports" && <Reports />}
