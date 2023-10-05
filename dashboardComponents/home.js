1918844f35ba4d880717a64a1198ea45e4b58071
jayasinp <pravinjayasinghe@gmail.com>
2023-10-05 18:19:42 +1100

    demo for demodashboard.js to debug dashboard.js

--- dashboardComponents/home.js
+++ dashboardComponents/home.js
@@ -3,10 +3,8 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">YOU ARE HOME</h5>
-      </div>
+    <div>
+      <h1>Welcome to the Dashboard</h1>
     </div>
   );
 }
---
+++ dashboardComponents/reports.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Reports() {
+  return (
+    <div>
+      <h1>You are at the Reports component</h1>
+    </div>
+  );
+}
+
+export default Reports;
